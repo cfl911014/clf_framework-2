b1ab7fd2b7afe5107e94c55b65e34692e59f5604
315778372@qq.com <315778372@qq.com>
2016-09-06 11:26:35 +0800

    Initial Commit

---
+++ one_navagation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  one_navagation
+//
+//  Created by mac  on 16/9/6.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ one_navagation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  one_navagation
+//
+//  Created by mac  on 16/9/6.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
